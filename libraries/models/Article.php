adaa7ced2516ba4b1d11346562082040806f822c
TeoGap <teogapillou@gmail.com>
2025-01-10 08:54:19 +0100

    Constructeur Article et Comment

--- libraries/models/Article.php
+++ libraries/models/Article.php
@@ -2,26 +2,31 @@
 require_once('libraries/database.php');
 class Article
 { 
+    private $pdo;
+    public function __construct()
+    {
+    $this->pdo = getPdo();
+    }
     public function findAll() : array
 { 
-    $pdo =getPdo();
-     $resultats = $pdo->query('SELECT * FROM articles ORDER BY created_at DESC'); 
+    
+     $resultats = $this->pdo->query('SELECT * FROM articles ORDER BY created_at DESC'); 
      $articles = $resultats->fetchAll(); 
      return $articles; 
 }
 
 public function find(int $id) 
  { 
-    $pdo =getPdo(); 
-    $query = $pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
+    
+    $query = $this->pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
     $query->execute(['article_id' => $id]);
     $article = $query->fetch();
      return $article; 
 }
 public function delete(int $id) : void
 {
-    $pdo = getPdo();
-    $query = $pdo->prepare('DELETE FROM articles WHERE id = :id');
+    
+    $query = $this->pdo->prepare('DELETE FROM articles WHERE id = :id');
     $query->execute(['id' => $id]);
 }
 
--- libraries/models/Comment.php
+++ libraries/models/Comment.php
@@ -2,30 +2,35 @@
 require_once('libraries/database.php');
 class Comment
 { 
+    private $pdo;
+    public function __construct()
+    {
+    $this->pdo = getPdo();
+    }
     public function findAllWithArticle() :array
     { 
-        $pdo =getPdo();
-         $resultats = $pdo->query('SELECT * FROM comments ORDER BY created_at DESC'); 
+        
+         $resultats = $this->pdo->query('SELECT * FROM comments ORDER BY created_at DESC'); 
          $commentaires = $resultats->fetchAll(); 
          return $commentaires; 
     }
     public function find(int $id) :array
     {
-        $pdo=getPdo();
-        $query = $pdo->prepare('SELECT * FROM comments WHERE id = :id');
+        
+        $query = $this->pdo->prepare('SELECT * FROM comments WHERE id = :id');
         $query->execute(['id' => $id]);
         $comment=$query->fetch(); return $comment; 
     }
     public function delete(int $id): void 
     {
-        $pdo=getPdo(); 
-        $query = $pdo->prepare('DELETE FROM comments WHERE id = :id');
+         
+        $query = $this->pdo->prepare('DELETE FROM comments WHERE id = :id');
         $query->execute(['id' => $id]); 
     }
     public function insert(string $author, string $content, int $article_id): void
     {
-        $pdo = getPdo();  
-        $query = $pdo->prepare('INSERT INTO comments SET author = :author, content = :content, article_id = :article_id, created_at = NOW()');
+          
+        $query = $this->pdo->prepare('INSERT INTO comments SET author = :author, content = :content, article_id = :article_id, created_at = NOW()');
         $query->execute(compact('author', 'content', 'article_id'));
     }
 }
